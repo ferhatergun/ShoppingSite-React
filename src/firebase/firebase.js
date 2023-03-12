46df9304827b79442dc5fd665890eeeab2076102
ferhatergun <123299806+ferhatergun@users.noreply.github.com>
2023-03-12 18:36:13 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_ZixiIjEqzyaTEpVUh3XbwYMGOaYixLM",
-  authDomain: "fakestore-2fd5f.firebaseapp.com",
-  projectId: "fakestore-2fd5f",
-  storageBucket: "fakestore-2fd5f.appspot.com",
-  messagingSenderId: "745596801896",
-  appId: "1:745596801896:web:800b1b827bc947584cfcdd",
-  measurementId: "G-2GLNQHVH12"
+  apiKey:YOUR İNFORMATİON
+  authDomain: YOUR İNFORMATİON
+  projectId: YOUR İNFORMATİON
+  storageBucket: YOUR İNFORMATİON
+  messagingSenderId: YOUR İNFORMATİON
+  appId: YOUR İNFORMATİON
+  measurementId:YOUR İNFORMATİON
 };
 
 
